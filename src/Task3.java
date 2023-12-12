760b724100cd585894c925d0e9ad80937ac38e9a
wladi <wlad@>
2023-12-12 18:53:46 +0300

    task3

--- src/Task3.java
+++ src/Task3.java
@@ -3,23 +3,23 @@ import java.util.Scanner;
 public class Task3 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String[] arr = new String [3];
+        String[] arr = new String[3];
         for (int i = 0; i < 3; i++) {
             arr[i] = scanner.nextLine();
         }
+        int sum = 0;
         for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3 - i -1; j++) {
-                if(arr[j].length() > arr[j+1].length()){
-                    String n = arr[j+1];
-                    arr[j+1] = arr[j];
-                    arr[j] = n;
-                }
-            }
+            sum += arr[i].length();
         }
 
-        for (int i = 0; i < arr.length/2; i++) {
-            System.out.println(arr[i]);
-            System.out.println("длина строки - " + arr[i].length());
+        sum /= 3;
+        System.out.println("средняя длина - " + sum);
+        System.out.println();
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i].length() < sum) {
+                System.out.println(arr[i]);
+                System.out.println("длина строки - " + arr[i].length());
+            }
         }
     }
 }
